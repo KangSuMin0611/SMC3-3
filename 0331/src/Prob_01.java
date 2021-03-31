f985c67a9453ab160c7b8d8c3f6c06603879b61e
jys <jyp360@naver.com>
2021-03-31 15:34:26 +0900

    Prob_01추가

---
+++ 0331/src/Prob_01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+//호수가 싫어하는 견과류, 버섯, 마요네즈
+
+class Grade{
+	
+	int math;
+	int sciece;
+	int english;
+		 	
+	public int average(){
+		return (math + sciece + english)/3;
+	}
+	
+}
+
+public class Prob_01 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		Grade me = new Grade(); 
+		System.out.print("수학, 과학 , 영어 순으로 3개의 점수입력>>");
+		
+		me.math=scan.nextInt();
+		me.sciece=scan.nextInt();
+		me.english=scan.nextInt();
+		
+		System.out.print("평균은 : " + me.average());
+		
+	}	
+}
+
