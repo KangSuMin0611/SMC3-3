7d1e806861c92be76bd2002eb5506c532068b23b
cn <cn0806@naver.com>
2021-04-14 15:16:39 +0900

    로또 프로그램 예시

---
+++ 0414/src/Numberr.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Numberr {
+
+	public static void main(String[] args) {
+		
+		Random ran = new Random();
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("10°³ÀÇ ·£´ý ¼ýÀÚ Áß ÇÏ³ª¸¦ ¸ÂÃçº¸¼¼¿ä(1 ~ 50): ");
+		int diff = 0;
+		int a = scan.nextInt();
+		int lotto[] = new int[10];
+		
+		for(int i=0; i<lotto.length; i++){
+				lotto[i] = (int)(Math.random()*50+1);
+				System.out.print(lotto[i]+" ");
+		}
+		
+		for(int j = 0; j<10; j++ ){
+			if(lotto[j]== a){
+				System.out.println("´çÃ·!");
+				diff=1;
+			}
+		}
+		if(diff==0){
+			System.out.println("²Î ¼ö¹Î¾Æ!!!!");
+		}
+	}
+}
